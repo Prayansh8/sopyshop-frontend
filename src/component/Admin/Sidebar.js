13f45f408ad7a6f04ee9492b79b0ce51ca435b45
Prayansh <prayansh811@gmail.com>
2023-05-17 17:20:13 +0530

    responsive css

--- src/component/Admin/Sidebar.js
+++ src/component/Admin/Sidebar.js
@@ -1,40 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <div className="h-100">
       <div className="d-flex flex-column flex-shrink-0 text-bg-dark h-100">
-        <Link
-          to="/admin/dashboard"
-          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-        >
-          <span className="fs-4">Admin Dashboard</span>
-        </Link>
-        <hr />
-        <ul className="nav nav-pills flex-column mb-auto">
-          <li>
-            <Link to="/admin/dashboard" className="nav-link text-white">
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/orders" className="nav-link text-white">
-              Orders
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/product" className="nav-link text-white">
-              Products
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/users" className="nav-link text-white">
-              Users
-            </Link>
-          </li>
-        </ul>
-        <hr />
+        <div className="desh-side">
+          <Link
+            to="/admin/dashboard"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+          >
+            <span className="desh-text">Admin Dashboard</span>
+          </Link>
+          <hr />
+          <ul className="nav nav-pills flex-column mb-auto">
+            <li>
+              <Link to="/admin/dashboard" className="nav-link text-white detx">
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/orders" className="nav-link text-white detx">
+                Orders
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/product" className="nav-link text-white detx">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/users" className="nav-link text-white detx">
+                Users
+              </Link>
+            </li>
+          </ul>
+          <hr />
+        </div>
       </div>
     </div>
   );
