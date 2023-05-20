8a9db0960ebcbb654e2be9c0b09b740c603c72ac
Prayansh <prayansh811@gmail.com>
2023-05-20 19:02:05 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const config = {
-  baseUrl: process.env.BASE_URI || "http://localhost:5000" || "http://sopyshop-env.eba-55ypccgw.ap-south-1.elasticbeanstalk.com",
+  baseUrl: process.env.BASE_URI || "http://localhost:5000",
 };
