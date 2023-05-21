49e09c7a12217ada9a4b91fb34073f3ddb7fdcf1
Prayansh <prayansh811@gmail.com>
2023-05-21 17:22:21 +0530

    update url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const config = {
-  baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000",
+  // baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000" ,
+  baseUrl: process.env.BASE_URI,
 };
