2a2ef3ecb833cb41c19ab867072a395a517612b7
Prayansh <prayansh811@gmail.com>
2023-05-22 13:31:04 +0530

    update footer

--- src/component/layout/Footer/Footer.js
+++ src/component/layout/Footer/Footer.js
@@ -6,58 +6,45 @@ import logo from "../../../images/logo.png";
 export default function Footer() {
   return (
     <>
-      <footer className="footer mt-auto py-3">
-        <div className="container">
-          <div className="row">
-            <div className="leftFooter w-25 col-sm-4 text-left m-auto">
-              <h1>Download our app</h1>
-              <p> Download app for andoroid and ios mobile phone.</p>
-              <a href="https://itunes.apple.com/us/app/apple-store/id375380948?mt=8">
-                <img
-                  src="https://linkmaker.itunes.apple.com/assets/shared/badges/en-us/appstore-lrg.svg"
-                  alt="Download on the App Store"
-                />
-              </a>
-              <a href="https://play.google.com/store/apps/details?id=com.example.app">
-                <img
-                  src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-                  alt="Get it on Google Play"
-                />
-              </a>
-            </div>
-            <div className="midFooter col-sm-4 w-50 m-auto text-center">
-              <img src={logo} alt="Company logo" />
-              <h1>Sopyshop</h1>
-              <p>Copyright © 2023 By Prayansh Gupta</p>
-            </div>
-            <div className="rightFooter col-sm-4 w-25 m-auto text-right">
-              <h1>Follow us on social media:</h1>
-              <div style={{ display: "inline-grid" }}>
-                <a
-                  href="https://twitter.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaTwitter /> Twitter
-                </a>
-                <a
-                  href="https://www.facebook.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaFacebook /> Facebook
-                </a>
-                <a
-                  href="https://www.instagram.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaInstagram /> Instagram
-                </a>
-              </div>
-            </div>
-          </div>
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 px-5 border-top bg-dark">
+        <div className="col-md-4 d-flex align-items-center">
+          <a
+            href="/"
+            className="mb-3 me-2 mb-md-0 color-white text-decoration-none lh-1"
+          >
+            <img
+              src={logo}
+              alt="Company logo"
+              className="logoimg color-white"
+            />
+          </a>
+          <span className="mb-3 mb-md-0 color-white">
+            © 2023 sopyshop, By Prayansh Gupta
+          </span>
         </div>
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3">
+            <a className="color-white" href="/">
+              <svg className="bi" width={24} height={24}>
+                <FaTwitter />
+              </svg>
+            </a>
+          </li>
+          <li className="ms-3">
+            <a className="color-white" href="/">
+              <svg className="bi" width={24} height={24}>
+                <FaInstagram />
+              </svg>
+            </a>
+          </li>
+          <li className="ms-3">
+            <a className="color-white" href="/">
+              <svg className="bi" width={24} height={24}>
+                <FaFacebook />
+              </svg>
+            </a>
+          </li>
+        </ul>
       </footer>
     </>
   );
