0673452a3d511cd4df23647e996e8a8e11c9c68b
Prayansh <prayansh811@gmail.com>
2023-05-25 18:30:47 +0530

    add stripe

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 export const config = {
-  // baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000",
-  baseUrl:
-    process.env.REACT_APP_API_BASE_URI ||
-    "http://sopyshop-env.eba-55ypccgw.ap-south-1.elasticbeanstalk.com",
+  baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000",
+  // baseUrl:
+  //   process.env.REACT_APP_API_BASE_URI ||
+  //   "http://sopyshop-env.eba-55ypccgw.ap-south-1.elasticbeanstalk.com",
+
+  stripe: {
+    stripeApi:
+      process.env.REACT_APP_STRIPE_API_KEY ||
+      "pk_test_51NBJjqSHHwsm4i0iP267XyzQXcD87NNlb17LHQBCZ4nTY7eCXF8O846cYWMraeTBkgmf9dbZpjdMQ5jzKAK9Vhmv00c5blnWIB",
+  },
 };
