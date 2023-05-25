2858f5670ec494f088fae8d962e2849a2406d88e
Prayansh <prayansh811@gmail.com>
2023-05-25 18:35:03 +0530

    add https url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 export const config = {
-  baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000",
-  // baseUrl:
-  //   process.env.REACT_APP_API_BASE_URI ||
-  //   "http://sopyshop-env.eba-55ypccgw.ap-south-1.elasticbeanstalk.com",
+  // baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000",
+  baseUrl: process.env.REACT_APP_API_BASE_URI || "https://sopyshop.worksnet.in",
 
   stripe: {
     stripeApi:
