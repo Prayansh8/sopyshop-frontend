1560e36646bc594cb6b7c3b1e3a941da7338f4f9
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2023-07-28 19:04:04 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 export const config = {
   // baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000",
-  baseUrl: process.env.REACT_APP_API_BASE_URI || "https://sopyshop.worksnet.in",
+  baseUrl: process.env.REACT_APP_API_BASE_URI,
 
   stripe: {
-    stripeApi:
-      process.env.REACT_APP_STRIPE_API_KEY ||
-      "pk_test_51NBJjqSHHwsm4i0iP267XyzQXcD87NNlb17LHQBCZ4nTY7eCXF8O846cYWMraeTBkgmf9dbZpjdMQ5jzKAK9Vhmv00c5blnWIB",
+    stripeApi: process.env.REACT_APP_STRIPE_API_KEY,
   },
 };
