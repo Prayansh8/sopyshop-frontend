68af97e18aaa8a2b2256451d8884a34a1f49ef5f
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2024-05-29 01:06:46 +0530

    push

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  // baseUrl: "http://localhost:5000",
-  baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000",
+  baseUrl: "http://localhost:5000",
+  // baseUrl: process.env.REACT_APP_API_BASE_URI || "http://localhost:5000",
   // baseUrl: process.env.REACT_APP_API_BASE_URI || "//sopyshop-env.eba-55ypccgw.ap-south-1.elasticbeanstalk.com/",
 
   stripe: {
