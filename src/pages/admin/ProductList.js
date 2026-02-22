bb3e37c9814e249a59f2dec9143e4a2587bbb9f0
Prayansh Gupta <prayansh811@gmail.com>
2026-02-22 22:51:04 +0530

    refactor: Remove empty react-router-dom import from admin list components.

--- src/pages/admin/ProductList.js
+++ src/pages/admin/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {} from "react-router-dom";
+
 import { DataGrid } from "@mui/x-data-grid";
 import { 
   Box, 
--- src/pages/admin/Users.js
+++ src/pages/admin/Users.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { deleteUserByAdmin, getAllUsers } from "../../redux/actions/userAction";
 import { useDispatch, useSelector } from "react-redux";
-import {} from "react-router-dom";
+
 import { DataGrid } from "@mui/x-data-grid";
 import { 
   Box, 
